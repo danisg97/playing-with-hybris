50688a1ea51f0d2ba9746d8356166a816518b14d
Dani Sánchez <sanchez.gracia.daniel@gmail.com>
2022-01-05 18:40:14 +0100

    Extend The Facade Layer

---
+++ bin/custom/concerttours/src/concerttours/facades/BandFacade.java
@@ -0,0 +1,11 @@
+package concerttours.facades;
+
+import java.util.List;
+
+import concerttours.data.BandData;
+
+public interface BandFacade {
+    BandData getBand(String name);
+
+    List<BandData> getBands();
+}
